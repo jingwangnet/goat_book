ed6056bb837e3e67847ef283a91074d777ab592f
jing Wang <hi@jingwang.me>
2024-02-06 21:55:08 +0800

    Use template to display

--- lists/tests.py
+++ lists/tests.py
@@ -5,15 +5,6 @@ from .views import home_page
 
 # Create your tests here.
 class HomePageTest(TestCase):
-    def test_home_page_return_correct_content(self):
-        request = HttpRequest()
-        respponse = home_page(request)
-        content = respponse.content.decode()
-
-        self.assertTrue(content.startswith("<html>"))
-        self.assertIn("<title>To-Do lists</title>", content)
-        self.assertTrue(content.endswith("</html>"))
-
-    def test_home_page_return_correct_content_2(self):
+    def test_home_page_use_home_template(self):
         response = self.client.get("/")
-        self.assertContains(response, "To-Do lists")
+        self.assertTemplateUsed(response, "lists/home.html")
