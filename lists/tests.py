648125f48015e4457a92f1f3f96910ec568f5b11
jing Wang <hi@jingwang.me>
2024-02-06 23:06:34 +0800

    Add List model

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.http import HttpRequest
 from .views import home_page
-from .models import Item
+from .models import Item, List
 
 
 # Create your tests here.
@@ -42,16 +42,24 @@ class ViewListTest(TestCase):
 
 class ItemModelTest(TestCase):
     def test_create_items_and_retrieves_later(self):
+        new_list = List()
+        new_list.save()
         first_item = Item()
         first_item.text = "First item"
+        first_item.list = new_list
         first_item.save()
 
         second_item = Item()
         second_item.text = "Second item"
+        second_item.list = new_list
         second_item.save()
 
+        self.assertEqual(1, List.objects.count())
         self.assertEqual(2, Item.objects.count())
-        first_saved_item, second_saved_item = Item.objects.all()
+        saved_list = List.objects.first()
+        first_saved_item, second_saved_item = saved_list.item_set.all()
 
         self.assertEqual(first_saved_item.text, "First item")
+        self.assertEqual(first_saved_item.list, new_list)
         self.assertEqual(second_saved_item.text, "Second item")
+        self.assertEqual(second_saved_item.list, new_list)
