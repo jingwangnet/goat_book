020d37f63cb33b06cf5b0edf308c312cc603bcd6
jing Wang <hi@jingwang.me>
2024-02-06 22:28:02 +0800

    Now can save post request and redirect after POST request

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.http import HttpRequest
 from .views import home_page
+from .models import Item
 
 
 # Create your tests here.
@@ -11,5 +12,28 @@ class HomePageTest(TestCase):
 
     def test_can_save_post_request(self):
         response = self.client.post("/", data={"new_item": "A new item"})
-        self.assertContains(response, "A new item")
-        self.assertTemplateUsed(response, "lists/home.html")
+        self.assertEqual(1, Item.objects.count())
+        item = Item.objects.first()
+        self.assertEqual(item.text, "A new item")
+        self.assertRedirects(response, "/")
+
+    def test_save_item_when_necessary(self):
+        response = self.client.get("/")
+        self.assertEqual(0, Item.objects.count())
+
+
+class ItemModelTest(TestCase):
+    def test_create_items_and_retrieves_later(self):
+        first_item = Item()
+        first_item.text = "First item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Second item"
+        second_item.save()
+
+        self.assertEqual(2, Item.objects.count())
+        first_saved_item, second_saved_item = Item.objects.all()
+
+        self.assertEqual(first_saved_item.text, "First item")
+        self.assertEqual(second_saved_item.text, "Second item")
