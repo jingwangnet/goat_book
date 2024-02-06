6b3470717ddc0559ed63f60bffacd7e1eb4f787d
jing Wang <hi@jingwang.me>
2024-02-06 21:50:41 +0800

    Update FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
+import time
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -16,3 +19,16 @@ class NewVisitorTest(LiveServerTestCase):
     def test_start_to_do_list(self):
         self.browser.get(self.live_server_url)
         self.assertIn("To-Do", self.browser.title)
+
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do lists", header_text)
+
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(input_box.get_attribute("plaeholder"), "Enter a to-do list")
+
+        input_box.send_keys("Buy peacook feather")
+        input_box.send_Keys(Keys.ENTER)
+
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn("1. Buy peacook feather", [row.text for row in rows])
