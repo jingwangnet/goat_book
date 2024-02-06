36ff3c9ab4ef8a0325281d8eaee83beb94e283a0
jing Wang <hi@jingwang.me>
2024-02-06 22:44:57 +0800

    Testing can save mutliple list on diffrernce URLS

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -53,3 +53,28 @@ class NewVisitorTest(LiveServerTestCase):
         self.submit_item("Make fly")
         self.wait_text_in_rows("1. Buy peacook feather")
         self.wait_text_in_rows("2. Make fly")
+
+    def test_create_multiple_lists_on_difference_urls(self):
+        self.browser.get(self.live_server_url)
+        self.submit_item("Buy peacook feather")
+        self.wait_text_in_rows("1. Buy peacook feather")
+
+        JIMMY_URL = self.browser.current_url
+        self.assertRegex(JIMMY_URL, "/lists/.+")
+
+        self.browser.delete_all_cookies()
+        self.browser.get(self.live_server_url)
+
+        page = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("Buy peacook feather", page)
+
+        self.submit_item("Buy milk")
+        self.wait_text_in_rows("1. Buy milk")
+
+        JOHN_URL = self.browser.current_url
+        self.assertRegex(JOHN_URL, "/lists/.+")
+        self.assertNotEqual(JIMMY_URL, JOHN_URL)
+
+        page = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("Buy peacook feather", page)
+        self.assertIn("Buy milk", page)
