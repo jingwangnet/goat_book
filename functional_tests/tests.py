6de1ef89c3d8facecac13cc3749ddf097cfa772a
jing Wang <hi@jingwang.me>
2024-02-12 10:51:44 +0800

    First steps of FT for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -78,3 +78,19 @@ class NewVisitorTest(LiveServerTestCase):
         page = self.browser.find_element(By.TAG_NAME, "body").text
         self.assertNotIn("Buy peacook feather", page)
         self.assertIn("Buy milk", page)
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEqual(
+            input_box.location["x"] + input_box.size["width"] / 2, 512, delta=30
+        )
+
+        self.submit_item("Buy peacook feather")
+
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEqual(
+            input_box.location["x"] + input_box.size["width"] / 2, 512, delta=30
+        )
