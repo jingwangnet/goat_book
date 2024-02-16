ad59e11d7e7dde4fc1d121fe839954107e8cac11
jing Wang <hi@jingwang.me>
2024-02-16 17:18:02 +0800

    Splite out UT into two files

---
+++ src/lists/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from lists.models import Item, List
+
+
+class ItemModelTest(TestCase):
+    def test_create_items_and_retrieves_later(self):
+        new_list = List()
+        new_list.save()
+        first_item = Item()
+        first_item.text = "First item"
+        first_item.list = new_list
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Second item"
+        second_item.list = new_list
+        second_item.save()
+
+        self.assertEqual(1, List.objects.count())
+        self.assertEqual(2, Item.objects.count())
+        saved_list = List.objects.first()
+        first_saved_item, second_saved_item = saved_list.item_set.all()
+
+        self.assertEqual(first_saved_item.text, "First item")
+        self.assertEqual(first_saved_item.list, new_list)
+        self.assertEqual(second_saved_item.text, "Second item")
+        self.assertEqual(second_saved_item.list, new_list)
--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.http import HttpRequest
-from .views import home_page
-from .models import Item, List
+from lists.views import home_page
+from lists.models import Item, List
 
 
 # Create your tests here.
@@ -75,28 +75,3 @@ class AddItemTest(TestCase):
 
     def test_redirect_after_post(self):
         self.assertRedirects(self.response, f"/lists/{self.list.pk}/")
-
-
-class ItemModelTest(TestCase):
-    def test_create_items_and_retrieves_later(self):
-        new_list = List()
-        new_list.save()
-        first_item = Item()
-        first_item.text = "First item"
-        first_item.list = new_list
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "Second item"
-        second_item.list = new_list
-        second_item.save()
-
-        self.assertEqual(1, List.objects.count())
-        self.assertEqual(2, Item.objects.count())
-        saved_list = List.objects.first()
-        first_saved_item, second_saved_item = saved_list.item_set.all()
-
-        self.assertEqual(first_saved_item.text, "First item")
-        self.assertEqual(first_saved_item.list, new_list)
-        self.assertEqual(second_saved_item.text, "Second item")
-        self.assertEqual(second_saved_item.list, new_list)
